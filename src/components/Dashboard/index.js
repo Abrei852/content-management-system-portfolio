075639a5e7c650ac1c5760ac0162be5689b2a9fa
abrei852 <abrahameishow95@gmail.com>
2021-12-23 13:22:53 +0100

    Moved Title component to Container component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "components/Dashboard/Header/Header.js";
-import ExperienceCard from "components/Dashboard/ExperienceCard/ExperienceCard";
-import Title from "components/Dashboard/Title/Title";
+import Container from "components/Dashboard/Container/Container";
 
 export default function Dashboard({ db, setToken }) {
     return (
         <div>
             <Header setToken={setToken} />
-            <Title h3="My Experience" cls="p-md-5" />
-            <ExperienceCard db={db} dbRef="myexp" cNameBg="bg-light" />
+            <Container db={db} dbRef="myexp" cNameBg="bg-light" />
         </div>
     );
 }
