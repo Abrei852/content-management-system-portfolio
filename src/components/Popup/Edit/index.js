d4474d5907787e407780e9deb4060f726ccdf86a
abrei852 <abrahameishow95@gmail.com>
2022-01-07 19:02:17 +0100

    Edit popup component

---
+++ src/components/Popup/Edit/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import Title from "components/Title";
+import { Button, Form } from "react-bootstrap";
+
+export default function PopupEdit({ close, object, updateObject }) {
+    const [formData, setFormData] = useState({
+        id: object.id,
+        title: object.val.title,
+        specs: object.val.specs,
+        icon: object.val.icon,
+    });
+    function handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        setFormData((v) => ({ ...v, [name]: value }));
+    }
+    function handleSubmit(event) {
+        event.preventDefault(event);
+        updateObject(formData);
+        close();
+    }
+    return (
+        <React.Fragment>
+            <Title h4={"Edit"} cls="p-1 text-center" />
+            <Form className="py-3 px-1 mt-3 border-top" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="title"
+                        required
+                        placeholder="Edit the title"
+                        value={formData.title}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicDesc">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="specs"
+                        required
+                        placeholder="Edit the Description"
+                        value={formData.specs}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicIcon">
+                    <Form.Label>Icon name (FA)</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="icon"
+                        required
+                        placeholder="Change icon"
+                        value={formData.icon}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+
+                <div className="d-flex justify-content-between pt-3">
+                    <Button
+                        title="Submit"
+                        variant="secondary"
+                        className="expand-sm"
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+                    <Button
+                        title="Cancel"
+                        variant="secondary"
+                        className="expand-sm"
+                        onClick={() => close()}
+                    >
+                        Cancel
+                    </Button>
+                </div>
+            </Form>
+        </React.Fragment>
+    );
+}
