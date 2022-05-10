1d7be654942812c7297255ea4e6887b0f9dd588a
abrei852 <abrahameishow95@gmail.com>
2022-05-10 23:19:39 +0200

    added styling for the header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,82 @@
+import "./style.css";
 import React from "react";
-import { Container, DropdownButton, Navbar } from "react-bootstrap";
-import Dropdown from "@restart/ui/esm/Dropdown";
+import { Container, DropdownButton, Dropdown, Navbar } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog, faSignOutAlt, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header({ signOut }) {
     return (
         <header>
             <Navbar bg="dark">
                 <Container className="">
-                    <Navbar.Brand href="/dashboard" className="text-white p-4 ">
+                    <Navbar.Brand href="/dashboard" className="text-white py-4">
                         Dashboard
                     </Navbar.Brand>
-                    <DropdownButton
-                        id="dropdown-item-button"
-                        title="Account.Name"
-                        variant="secondary"
-                        className="justify-content-end d-flex"
-                    >
+                    <DropdownButton id="" title="Name" variant="secondary">
                         <Dropdown.Item
-                            className="btn btn-block"
+                            className="btn btn-block w-100 rounded-0"
                             as="button"
+                            title="Account"
+                        >
+                            <div className="d-flex justify-content-between align-items-center">
+                                <h6
+                                    style={{
+                                        marginBottom: 0 + "px",
+                                        fontSize: 12 + "px",
+                                        fontWeight: "normal",
+                                    }}
+                                >
+                                    Account
+                                </h6>
+                                <FontAwesomeIcon
+                                    icon={faUser}
+                                    style={{ fontSize: 12 + "px" }}
+                                />
+                            </div>
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            className="rotate"
+                            as="button"
+                            title="settings"
+                        >
+                            <div className="d-flex justify-content-between align-items-center">
+                                <h6
+                                    style={{
+                                        marginBottom: 0 + "px",
+                                        fontSize: 12 + "px",
+                                        fontWeight: "normal",
+                                    }}
+                                >
+                                    Settings
+                                </h6>
+                                <FontAwesomeIcon
+                                    icon={faCog}
+                                    style={{ fontSize: 12 + "px" }}
+                                    className="gear"
+                                />
+                            </div>
+                        </Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item
+                            className="btn btn-block w-100 rounded-0"
+                            as="button"
+                            title="Sign Out"
                             onClick={() => signOut()}
                         >
-                            Sign Out
+                            <div className="d-flex justify-content-between align-items-center">
+                                <h6
+                                    style={{
+                                        marginBottom: 0 + "px",
+                                        fontSize: 12 + "px",
+                                    }}
+                                >
+                                    Sign out
+                                </h6>
+                                <FontAwesomeIcon
+                                    icon={faSignOutAlt}
+                                    style={{ fontSize: 12 + "px" }}
+                                />
+                            </div>
                         </Dropdown.Item>
                     </DropdownButton>
                 </Container>
