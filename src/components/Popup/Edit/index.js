07e63778ee1e7e5ed8bb730b7022732a137dc7de
abrei852 <abrahameishow95@gmail.com>
2022-05-15 23:23:04 +0200

    code cleanup

--- src/components/Popup/Edit/index.js
+++ src/components/Popup/Edit/index.js
@@ -1,107 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import Title from "components/Title";
-import { Button, Form } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-export default function PopupEdit({ close, object, editItemDb }) {
-    const [formData, setFormData] = useState({
-        id: object.id,
-        title: object.val.title,
-        specs: object.val.specs,
-        overline: object.val.overline,
-        date: object.val.date,
-    });
-    function handleChange(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-        setFormData((v) => ({ ...v, [name]: value }));
-    }
-    function handleSubmit(event) {
-        event.preventDefault(event);
-        editItemDb(formData);
-        close();
-    }
+export default function PopupEdit({
+    children,
+    handleChange,
+    onSubmit,
+    object,
+    close,
+}) {
     return (
         <React.Fragment>
             <Title h4={"Edit"} cls="p-1 text-center" />
-            <Form className="py-3 px-1 mt-3 border-top" onSubmit={handleSubmit}>
-                <div className="d-flex">
-                    <Form.Group className="mb-3 px-2">
-                        <Form.Group className="mb-3">
-                            <Form.Label>Overline</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="overline"
-                                required
-                                placeholder="Edit the Overline"
-                                value={formData.overline}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="title"
-                            required
-                            placeholder="Edit the title"
-                            value={formData.title}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3 px-2">
-                        <Form.Group className="mb-3">
-                            <Form.Label>Date</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="date"
-                                required
-                                placeholder="Edit the Date"
-                                value={formData.date}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="specs"
-                            required
-                            placeholder="Edit the Description"
-                            value={formData.specs}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
-                </div>
-
-                {/* <Form.Group className="mb-3" controlId="formBasicIcon">
-                    <Form.Label>Icon name (FA)</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="icon"
-                        placeholder="Change icon"
-                        value={formData.icon}
-                        onChange={handleChange}
-                    />
-                </Form.Group> */}
-
-                <div className="d-flex justify-content-between pt-3 px-2">
-                    <Button
-                        title="Submit"
-                        variant="success"
-                        className="expand-sm"
-                        type="submit"
-                    >
-                        Submit
-                    </Button>
-                    <Button
-                        title="Cancel"
-                        variant="danger"
-                        className="expand-sm"
-                        onClick={() => close()}
-                    >
-                        Cancel
-                    </Button>
-                </div>
-            </Form>
+            {React.cloneElement(children, {
+                handleChange: handleChange,
+                onSubmit: onSubmit,
+                object: object,
+                close: close,
+            })}
         </React.Fragment>
     );
 }
+
+PopupEdit.propTypes = {
+    handleChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    object: PropTypes.object.isRequired,
+    close: PropTypes.func.isRequired,
+};
