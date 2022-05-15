0690c99e0b4e6188ab3406a0bed49b52d4836262
abrei852 <abrahameishow95@gmail.com>
2022-05-15 02:57:04 +0200

    added another contentcontainer

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,6 +3,7 @@ import ContentContainer from "components/ContentContainer/index";
 import Header from "components/Header/index";
 import { signOutUser } from "db/firebase";
 import CardExpandable from "components/Card/Expandable";
+import CardExpandableWithImage from "components/Card/ExpandableWithImage";
 
 export default function Dashboard() {
     function signOut() {
@@ -14,13 +15,18 @@ export default function Dashboard() {
             <Header signOut={signOut} />
             <div className="d-flex justify-content-around">
                 <ContentContainer dbRef="workexp/" hTitle="Work Experience">
-                        <CardExpandable />
+                    <CardExpandable />
                 </ContentContainer>
                 <ContentContainer
                     dbRef="eduexp/"
                     hTitle="Educational Experience"
                 >
                     <CardExpandable />
+                </ContentContainer>
+            </div>
+            <div className="d-flex justify-content-around">
+                <ContentContainer dbRef="projects/" hTitle="Projects">
+                    <CardExpandableWithImage />
                 </ContentContainer>
             </div>
         </React.Fragment>
