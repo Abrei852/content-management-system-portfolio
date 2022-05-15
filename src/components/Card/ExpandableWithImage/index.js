bcc37d05a6f477f0f33c946b0c32a72a49ccf356
abrei852 <abrahameishow95@gmail.com>
2022-05-15 17:07:40 +0200

    removed unused id tag

--- src/components/Card/ExpandableWithImage/index.js
+++ src/components/Card/ExpandableWithImage/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function CardExpandableWithImage({ object }) {
     return (
         <div className="p-2">
-            <img id="myimg" src={object.val.img} className="image" />
+            <img src={object.val.img} className="image" />
             <div className="d-flex justify-content-between align-items-center m-8">
                 <p>{object.val.name}</p>
                 <p>+</p>
