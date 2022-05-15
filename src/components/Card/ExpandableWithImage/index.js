cf88d13ee79a5d81711d71bdb9caaa76ec391381
abrei852 <abrahameishow95@gmail.com>
2022-05-15 17:57:30 +0200

    retreive data languages from database

--- src/components/Card/ExpandableWithImage/index.js
+++ src/components/Card/ExpandableWithImage/index.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
 
 export default function CardExpandableWithImage({ object }) {
+    // console.log(object.val.langs.length)
     return (
         <div className="p-2">
             <img src={object.val.img} className="image" />
-            <div className="d-flex justify-content-between align-items-center m-8">
+            <div className="d-flex">
+                {object.val.langs ? (
+                    object.val.langs.map((language, idx) => (
+                        <div key={idx} className="mt-1 lang-container">
+                            <p className="font-12 lang">{language}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p></p>
+                )}
+            </div>
+            <div className="d-flex justify-content-between align-items-center mt-2">
                 <p>{object.val.name}</p>
                 <p>+</p>
             </div>
+
+            <div>{object.val.specs}</div>
         </div>
     );
 }
