526764bcc66a23028768273577fbbbb4fac0ed57
abrei852 <abrahameishow95@gmail.com>
2022-08-21 14:27:43 +0200

    added auth methods and formatted code

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,45 +3,46 @@ import CardExpandableWithImage from "components/Card/ExpandableWithImage";
 import CardExpandable from "components/Card/Expandable";
 import ContentContainer from "components/Content/index";
 import Header from "components/Header/index";
-import { signOutUser } from "db/firebase";
+import { firebaseAuth } from "db/firebase";
+import { signOut } from "firebase/auth";
 
 export default function Dashboard() {
-    function signOut() {
-        signOutUser();
-    }
+	const signOutUser = () => {
+		signOut(firebaseAuth);
+	};
 
-    return (
-        <React.Fragment>
-            <Header signOut={signOut} />
-            <div className="d-flex justify-content-around">
-                <ContentContainer
-                    loc="workexp/"
-                    hTitle="Work Experience"
-                    clsContainer="cont-container-exp"
-                    clsCard="card-exp"
-                >
-                    <CardExpandable />
-                </ContentContainer>
-                <ContentContainer
-                    loc="eduexp/"
-                    hTitle="Educational Experience"
-                    clsContainer="cont-container-exp"
-                    clsCard="card-exp"
-                >
-                    <CardExpandable />
-                </ContentContainer>
-            </div>
-            <div className="">
-                <ContentContainer
-                    loc="projects/"
-                    hTitle="Projects"
-                    clsContainer="cont-container-cwi"
-                    clsCardContainer="card-container"
-                    clsCard="card-cwi"
-                >
-                    <CardExpandableWithImage />
-                </ContentContainer>
-            </div>
-        </React.Fragment>
-    );
+	return (
+		<React.Fragment>
+			<Header signOut={signOutUser} />
+			<div className="d-flex justify-content-around">
+				<ContentContainer
+					loc="workexp/"
+					hTitle="Work Experience"
+					clsContainer="cont-container-exp"
+					clsCard="card-exp"
+				>
+					<CardExpandable />
+				</ContentContainer>
+				<ContentContainer
+					loc="eduexp/"
+					hTitle="Educational Experience"
+					clsContainer="cont-container-exp"
+					clsCard="card-exp"
+				>
+					<CardExpandable />
+				</ContentContainer>
+			</div>
+			<div className="">
+				<ContentContainer
+					loc="projects/"
+					hTitle="Projects"
+					clsContainer="cont-container-cwi"
+					clsCardContainer="card-container"
+					clsCard="card-cwi"
+				>
+					<CardExpandableWithImage />
+				</ContentContainer>
+			</div>
+		</React.Fragment>
+	);
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,56 +1,72 @@
 import React, { useState } from "react";
-import { signInUser } from "db/firebase";
+import { firebaseAuth } from "db/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import "./style.css";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
-    };
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		clearForm();
+	};
 
-    const signIn = async (email, password) => {
-        await signInUser(email, password);
-    };
+	const signIn = async (email, password) => {
+		try {
+			await signInWithEmailAndPassword(firebaseAuth, email, password);
+		} catch (error) {
+			setError("Wrong username or password");
+		}
+	};
 
-    return (
-        <div className="body-logn">
-            <div className="cntr-logn">
-                <h1>Log in page</h1>
-                <form onSubmit={onSubmit} className="form">
-                    <div>
-                        <div className="inpt">
-                            <label>
-                                <input
-                                    type="text"
-                                    placeholder="Email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </label>
-                            <label>
-                                <input
-                                    type="password"
+	const clearForm = () => {
+		setEmail("");
+		setPassword("");
+		setError("");
+	};
+
+	return (
+		<div className="body-logn">
+			<div className="cntr-logn">
+				<h1>Log in page</h1>
+				<form onSubmit={onSubmit} className="form">
+					<div>
+						<div className="inpt">
+							<label>
+								<input
+									type="text"
+									placeholder="Email"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</label>
+							<label>
+								<input
+									type="password"
 									placeholder="Password"
-                                    value={password}
-                                    onChange={(e) =>
-                                        setPassword(e.target.value)
-                                    }
-                                />
-                            </label>
-                        </div>
-                        <div>
-                            <button
-                                type="submit"
-                                onClick={() => signIn(email, password)}
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+									value={password}
+									onChange={(e) =>
+										setPassword(e.target.value)
+									}
+								/>
+							</label>
+						</div>
+						<div className="pb-3">
+							<p>{error}</p>
+						</div>
+						<div>
+							<button
+								type="submit"
+								onClick={() => signIn(email, password)}
+							>
+								Submit
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
