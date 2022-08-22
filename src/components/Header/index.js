b6433377a74b9d86d31fb398732b74cc3fd4aa80
abrei852 <abrahameishow95@gmail.com>
2022-08-22 02:15:24 +0200

    added cv uploader component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,86 +1,104 @@
 import "./style.css";
 import React from "react";
-import { Container, DropdownButton, Dropdown, Navbar } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PopUpTrigger from "components/PopUpTrigger";
+import {
+	Container,
+	DropdownButton,
+	Dropdown,
+	Navbar,
+	Button,
+} from "react-bootstrap";
 import { faCog, faSignOutAlt, faUser } from "@fortawesome/free-solid-svg-icons";
+import { firebaseStorage } from "db/firebase";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header({ signOut }) {
-    return (
-        <header>
-            <Navbar bg="dark">
-                <Container className="">
-                    <Navbar.Brand href="/dashboard" className="text-white py-4">
-                        Dashboard
-                    </Navbar.Brand>
-                    <DropdownButton id="" title="Name" variant="secondary">
-                        <Dropdown.Item
-                            className="btn btn-block w-100 rounded-0"
-                            as="button"
-                            title="Account"
-                        >
-                            <div className="d-flex justify-content-between align-items-center">
-                                <h6
-                                    style={{
-                                        marginBottom: 0 + "px",
-                                        fontSize: 12 + "px",
-                                        fontWeight: "normal",
-                                    }}
-                                >
-                                    Account
-                                </h6>
-                                <FontAwesomeIcon
-                                    icon={faUser}
-                                    style={{ fontSize: 12 + "px" }}
-                                />
-                            </div>
-                        </Dropdown.Item>
-                        <Dropdown.Item
-                            className="rotate"
-                            as="button"
-                            title="settings"
-                        >
-                            <div className="d-flex justify-content-between align-items-center">
-                                <h6
-                                    style={{
-                                        marginBottom: 0 + "px",
-                                        fontSize: 12 + "px",
-                                        fontWeight: "normal",
-                                    }}
-                                >
-                                    Settings
-                                </h6>
-                                <FontAwesomeIcon
-                                    icon={faCog}
-                                    style={{ fontSize: 12 + "px" }}
-                                    className="gear"
-                                />
-                            </div>
-                        </Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item
-                            className="btn btn-block w-100 rounded-0"
-                            as="button"
-                            title="Sign Out"
-                            onClick={() => signOut()}
-                        >
-                            <div className="d-flex justify-content-between align-items-center">
-                                <h6
-                                    style={{
-                                        marginBottom: 0 + "px",
-                                        fontSize: 12 + "px",
-                                    }}
-                                >
-                                    Sign out
-                                </h6>
-                                <FontAwesomeIcon
-                                    icon={faSignOutAlt}
-                                    style={{ fontSize: 12 + "px" }}
-                                />
-                            </div>
-                        </Dropdown.Item>
-                    </DropdownButton>
-                </Container>
-            </Navbar>
-        </header>
-    );
+	return (
+		<header>
+			<Navbar bg="dark">
+				<Container className="py-4">
+					<Navbar.Brand href="/dashboard" className="text-white">
+						Dashboard
+					</Navbar.Brand>
+					<div className="d-flex align-items-center">
+						<PopUpTrigger firebaseStorage={firebaseStorage}>
+							<Button
+								title="Update CV"
+								className="cv-picker expand-sm"
+							>
+								Update CV
+							</Button>
+						</PopUpTrigger>
+						<DropdownButton id="" title="Name" variant="secondary">
+							<Dropdown.Item
+								className="btn btn-block w-100 rounded-0"
+								as="button"
+								title="Account"
+							>
+								<div className="d-flex justify-content-between align-items-center">
+									<h6
+										style={{
+											marginBottom: 0 + "px",
+											fontSize: 12 + "px",
+											fontWeight: "normal",
+										}}
+									>
+										Account
+									</h6>
+									<FontAwesomeIcon
+										icon={faUser}
+										style={{ fontSize: 12 + "px" }}
+									/>
+								</div>
+							</Dropdown.Item>
+							<Dropdown.Item
+								className="rotate"
+								as="button"
+								title="settings"
+							>
+								<div className="d-flex justify-content-between align-items-center">
+									<h6
+										style={{
+											marginBottom: 0 + "px",
+											fontSize: 12 + "px",
+											fontWeight: "normal",
+										}}
+									>
+										Settings
+									</h6>
+									<FontAwesomeIcon
+										icon={faCog}
+										style={{ fontSize: 12 + "px" }}
+										className="gear"
+									/>
+								</div>
+							</Dropdown.Item>
+							<Dropdown.Divider />
+							<Dropdown.Item
+								className="btn btn-block w-100 rounded-0"
+								as="button"
+								title="Sign Out"
+								onClick={() => signOut()}
+							>
+								<div className="d-flex justify-content-between align-items-center">
+									<h6
+										style={{
+											marginBottom: 0 + "px",
+											fontSize: 12 + "px",
+										}}
+									>
+										Sign out
+									</h6>
+									<FontAwesomeIcon
+										icon={faSignOutAlt}
+										style={{ fontSize: 12 + "px" }}
+									/>
+								</div>
+							</Dropdown.Item>
+						</DropdownButton>
+					</div>
+				</Container>
+			</Navbar>
+		</header>
+	);
 }
