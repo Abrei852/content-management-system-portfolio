4f0f548d860830c080e31643402b2ab51a4a9fec
abrei852 <abrahameishow95@gmail.com>
2023-01-03 20:08:06 +0100

    update

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,18 +2,11 @@ import React from "react";
 import CardExpandableWithImage from "components/Card/ExpandableWithImage";
 import CardExpandable from "components/Card/Expandable";
 import ContentContainer from "components/Content/index";
-import Header from "components/Header/index";
-import { firebaseAuth } from "db/firebase";
-import { signOut } from "firebase/auth";
 
 export default function Dashboard() {
-	const signOutUser = () => {
-		signOut(firebaseAuth);
-	};
 
 	return (
 		<React.Fragment>
-			<Header signOut={signOutUser} />
 			<div className="d-flex justify-content-around">
 				<ContentContainer
 					loc="workexp/"
