f4add4434d8b61de804f49ba650acdab3050c1f7
abrei852 <abrahameishow95@gmail.com>
2023-01-07 03:47:26 +0100

    styling edit and new props

--- src/components/Popup/Option/index.js
+++ src/components/Popup/Option/index.js
@@ -5,25 +5,29 @@ import { Button } from "react-bootstrap";
 export default function PopupOption({
     btnClick,
     btnTitle1,
+    btnClass1,
+    btnDisabled1,
     btnTitle2,
+    btnClass2,
     btnType1,
     close,
 }) {
     return (
-        <div className="d-flex justify-content-between padding-top-20">
+        <div className="d-flex justify-content-between">
             <Button
                 title={btnTitle1}
                 variant="success"
-                className="expand-sm font-14"
+                className={`expand-sm font-14 ${btnClass1}`}
                 type={btnType1}
                 onClick={btnClick}
+                disabled={btnDisabled1}
             >
                 {btnTitle1}
             </Button>
             <Button
                 title={btnTitle2}
                 variant="danger"
-                className="expand-sm font-14"
+                className={`expand-sm font-14 ${btnClass2}`}
                 onClick={() => close()}
             >
                 {btnTitle2}
