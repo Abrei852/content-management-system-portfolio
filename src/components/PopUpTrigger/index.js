1319c99b670aba3046f84f6da1016a6165bd8feb
abrei852 <abrahameishow95@gmail.com>
2023-01-08 22:49:49 +0100

    added new parameter to component

--- src/components/PopUpTrigger/index.js
+++ src/components/PopUpTrigger/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import Popup from "reactjs-popup";
 import Upload from "components/Content/Upload";
 
-export default function PopUpTrigger({ children, firebaseStorage }) {
+export default function PopUpTrigger({ children, firebaseStorage, firebaseFolder }) {
 	return (
 		<div>
 			<Popup modal trigger={children} lockScroll>
 				{(close) => (
 					<React.Fragment>
-						<Upload close={close} firebaseStorage={firebaseStorage} firebaseFolder={"cv"}/>
+						<Upload close={close} firebaseStorage={firebaseStorage} firebaseFolder={firebaseFolder}/>
 					</React.Fragment>
 				)}
 			</Popup>
